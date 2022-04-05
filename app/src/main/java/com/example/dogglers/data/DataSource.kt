5bf7267966f11324a7a7cd2420401fb3327b031e
Alkusr <68933036+alkusr@users.noreply.github.com>
2022-04-06 03:08:24 +0800

    First commit

---
+++ app/src/main/java/com/example/dogglers/data/DataSource.kt
@@ -0,0 +1,72 @@
+
+package com.example.dogglers.data
+
+import com.example.dogglers.R
+import com.example.dogglers.model.Mouse
+
+object DataSource {
+
+    val mouses: List<Mouse> = listOf(
+        Mouse(
+            R.drawable.mousea,
+            "Logitech A",
+            "Pop Blast 1",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mouseb,
+            "Logitech B",
+            "M650",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mousece,
+            "Logitech C",
+            "M590",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.moused,
+            "Logitech D",
+            "MX Master 3",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mousee,
+            "Logitech E",
+            "MX Anywhere 3",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mousef,
+            "Logitech F",
+            "M171",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mouseg,
+            "Logitech G",
+            "M100R",
+            "Cable"
+        ),
+        Mouse(
+            R.drawable.mouseh,
+            "Logitech H",
+            "Ergo M575",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mousei,
+            "Logitech I",
+            "M220",
+            "Wireless"
+        ),
+        Mouse(
+            R.drawable.mousej,
+            "Logitech J",
+            "M330",
+            "Wireless"
+        )
+
+    )
+}
